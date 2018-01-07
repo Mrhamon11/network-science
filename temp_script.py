477c89b5af247ae00ce0e33669eef37c8454cdea
yehudagale <yehuda.gale@gmail.com>
2018-01-06 23:57:13 -0500

    added new network

--- temp_script.py
+++ temp_script.py
@@ -1,13 +1,14 @@
 import pickle
-input_file = open("./temp.txt", 'rb')
+from sys import argv
+input_file = open(argv[1], 'rb')
 temp_list = pickle.load(input_file)
 com_pair = temp_list[10]
-input_file = open("./firstcom.txt", 'r')
-output_file = open("./both_communitites.txt", "w")
+input_file = open(argv[2], 'r')
+output_file = open(argv[3], "w")
 for line in input_file:
-     if "#" not in line:
+     if ":" in line and "#" not in line:
              items = line.split()
-             Louvain_community = com_pair[1][int(items[0])] 
+             Louvain_community = com_pair[1][int(items[3])] 
              line2 = line + " " + str(Louvain_community) + " " + str(len(com_pair[0][Louvain_community]))
              line2 = line2.replace("\n","")
              line2 += "\n"

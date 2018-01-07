d6162e81befa53311be9dc55368344e040890b1e
yehudagale <yehuda.gale@gmail.com>
2018-01-07 00:21:46 -0500

    added actual

--- temp_script.py
+++ temp_script.py
@@ -3,6 +3,7 @@ from sys import argv
 input_file = open(argv[1], 'rb')
 temp_list = pickle.load(input_file)
 com_pair = temp_list[10]
+	
 input_file = open(argv[2], 'r')
 output_file = open(argv[3], "w")
 input_file2 = open(argv[4], 'r')
@@ -10,6 +11,8 @@ actual_dict = {}
 for line in input_file2:
 	items = line.split()
 	actual_dict[int(items[0])] = items[1]
+new_list = []
+
 for line in input_file:
      if ":" in line and "#" not in line:
              items = line.split()
@@ -18,5 +21,13 @@ for line in input_file:
              line2 = line + " " + str(Louvain_community)  + " actual:" + actual_dict[com_pair[1][int(items[3])]]
              line2 = line2.replace("\n","")
              line2 += "\n"
-             print(line2)
-             output_file.write(line2)
+             # print(line2)
+             new_list.append(line2)
+def find_group(x):
+	split = x.split(":")
+	print(split)
+	return int(split[2])
+new_list.sort(key=find_group)
+output_file.close
+for item in new_list:
+	output_file.write(item)
